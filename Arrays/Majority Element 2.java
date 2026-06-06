6ffa59158612fd1a66dfd868518ea6e6495befa4
Aarya Gonjari <gonjariaarya379@gmail.com>
2026-06-06 19:16:20 +0530

    Refactor majorityElement to use HashMap approach

--- Arrays/Majority Element 2.java
+++ Arrays/Majority Element 2.java
@@ -1,43 +1,20 @@
 class Solution {
-public:
-    vector<int> majorityElement(vector<int>& nums) {
-        int candidate1 = 0, candidate2 = 0;
-        int count1 = 0, count2 = 0;
+    public List<Integer> majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Integer> ans = new ArrayList<>();
 
         for (int num : nums) {
-            if (num == candidate1) {
-                count1++;
-            }
-            else if (num == candidate2) {
-                count2++;
-            }
-            else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            }
-            else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            }
-            else {
-                count1--;
-                count2--;
-            }
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        count1 = count2 = 0;
+        int limit = nums.length / 3;
 
-        for (int num : nums) {
-            if (num == candidate1) count1++;
-            else if (num == candidate2) count2++;
+        for (int key : map.keySet()) {
+            if (map.get(key) > limit) {
+                ans.add(key);
+            }
         }
-
-        vector<int> ans;
-        int limit = nums.size() / 3;
-
-        if (count1 > limit) ans.push_back(candidate1);
-        if (count2 > limit) ans.push_back(candidate2);
 
         return ans;
     }
-};
+}
