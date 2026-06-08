e51f373f4b00e0d7219f39624c74272866c3c9f0
Aarya Gonjari <gonjariaarya379@gmail.com>
2026-06-08 12:28:28 +0530

    Implement subarray count with given XOR K

---
+++ Arrays/Count subarrays with given xor K.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int subarraysWithXorK(int[] nums, int k) {
+      HashMap<Integer,Integer> map=new HashMap<>();  
+      int xr=0;
+      int count=0;
+      map.put(0,1);
+      for(int num:nums){
+        xr^=num;
+        int target=xr^k;
+        count+=map.getOrDefault(target,0);
+        map.put(xr,map.getOrDefault(xr,0)+1);
+
+      }
+        return count;
+
+    }
+}
